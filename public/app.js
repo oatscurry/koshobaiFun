7f19c79d5937b8170589469cf07eeadbf43cde5f
Tariq Lacy <koshobai@rodin.home>
2020-04-12 21:37:23 +0900

    working out signup submission issue

--- public/app.js
+++ public/app.js
@@ -41,9 +41,9 @@ app.post("/", function(req, res){
   const jsonData = JSON.stringify(data);
   const request = https.request(url, options, function(response){
     if(response.statusCode === 200){
-      res.sendFile(__dirname + "/success.html");
+      res.sendFile(__dirname + "../success.html");
     } else {
-      res.sendFile(__dirname + "/failure.html");
+      res.sendFile(__dirname + "../failure.html");
     }
     response.on("data", function(data){
       console.log(JSON.parse(data));
